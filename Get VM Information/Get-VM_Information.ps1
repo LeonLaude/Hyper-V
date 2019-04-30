9f3e563f1983adadb8ceb7e8fe41c204ada75932
Leon Laude <44682947+LeonLaude@users.noreply.github.com>
2019-04-30 18:35:38 +0200

    Update Get-VM_Information.ps1

--- Get VM Information/Get-VM_Information.ps1
+++ Get VM Information/Get-VM_Information.ps1
@@ -9,3 +9,4 @@
 $Server = Read-Host "Please enter your Hyper-V server name"
 $LogLocation = Read-Host "Please enter a location to store the HTML report"
 Get-VM -Computername $Server | Select VMName, State, Version, ProcessorCount, DynamicMemoryEnabled, @{n="Startup(GB)";e={$_.memorystartup/1GB}}, @{n="Minimum(GB)";e={$_.memoryminimum/1GB}}, @{n="Maximum(GB)";e={$_.memorymaximum/1GB}} | ConvertTo-HTML > $LogLocation\VM-Information.html
+
