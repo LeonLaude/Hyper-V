5595eed18f5d500e194b9644877cbbcb69340884
Leon Laude <44682947+LeonLaude@users.noreply.github.com>
2019-04-30 18:33:49 +0200

    Add files via upload

---
+++ Get VM Information/Get-VM_Information.ps1
@@ -0,0 +1,3 @@
+﻿$Server = Read-Host "Please enter your Hyper-V server name"
+$LogLocation = Read-Host "Please enter a location to store the HTML report"
+Get-VM -Computername $Server | Select VMName, State, Version, ProcessorCount, DynamicMemoryEnabled, @{n="Startup(GB)";e={$_.memorystartup/1GB}}, @{n="Minimum(GB)";e={$_.memoryminimum/1GB}}, @{n="Maximum(GB)";e={$_.memorymaximum/1GB}} | ConvertTo-HTML > $LogLocation\VM-Information.html
