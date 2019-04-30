4ee5ee7ec637ae5336c879e582c0ce444c113fd0
Leon Laude <44682947+LeonLaude@users.noreply.github.com>
2019-04-30 18:35:21 +0200

    Update Get-VM_Information.ps1

--- Get VM Information/Get-VM_Information.ps1
+++ Get VM Information/Get-VM_Information.ps1
@@ -1,3 +1,11 @@
-﻿$Server = Read-Host "Please enter your Hyper-V server name"
+# --------------------------------
+# | Author: Leon Laude           |
+# | Email:  leon.laude@gmail.com |
+# --------------------------------
+#
+# This script retrieves the virtual machine information on the Hyper-V host that you define in the script.
+#
+
+$Server = Read-Host "Please enter your Hyper-V server name"
 $LogLocation = Read-Host "Please enter a location to store the HTML report"
 Get-VM -Computername $Server | Select VMName, State, Version, ProcessorCount, DynamicMemoryEnabled, @{n="Startup(GB)";e={$_.memorystartup/1GB}}, @{n="Minimum(GB)";e={$_.memoryminimum/1GB}}, @{n="Maximum(GB)";e={$_.memorymaximum/1GB}} | ConvertTo-HTML > $LogLocation\VM-Information.html
